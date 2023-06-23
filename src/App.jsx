8ed7e71568ef431d2d7ae686083eb669b5935415
rabobahago <rabobahago@gmail.com>
2023-06-23 15:12:30 +0100

    react router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./components/Product";
+import Pricing from "./components/Pricing";
+import HomePage from "./components/HomePage";
+import PageNotFound from "./components/PageNotFound";
+
 function App() {
   return (
-    <>
-      <h1>Hello world!</h1>
-    </>
+    <div>
+      <h3>Hello world!</h3>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
