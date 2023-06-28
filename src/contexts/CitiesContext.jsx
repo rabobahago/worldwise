2c970060c874fb62347cff4564c2b37e4a740a02
rabobahago <rabobahago@gmail.com>
2023-06-28 17:46:44 +0100

    fetched single city data from api call

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -5,6 +5,7 @@ const CitiesContext = createContext();
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
   useState(function () {
     async function fetchData() {
       try {
@@ -20,8 +21,20 @@ function CitiesProvider({ children }) {
     }
     fetchData();
   }, []);
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch {
+      alert("There is an error loading the data");
+    } finally {
+      setIsLoading(false);
+    }
+  }
   return (
-    <CitiesContext.Provider value={{ cities, isLoading }}>
+    <CitiesContext.Provider value={{ cities, isLoading, currentCity, getCity }}>
       {children}
     </CitiesContext.Provider>
   );
